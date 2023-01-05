190f4a2968ca0a785f0419e71fc3cc8b8c254d0e
luis-luciano <luis.luciano.morales@gmail.com>
2023-01-05 17:04:04 -0600

    feat: Se agrega Mockery library
    - Se utiliza la libreria mockery, para la creacion de mocks o stubs para simular el comportamiento de las clases. El uso de esta tecnica ayuda en realizar pruebas unitarias mas asladas y solventar facilmente las dependencias.
    - Se cambia el uso de AuthenticatorStub creado anteriormente para usar Mockery, esto evitaria crear nuestros stubs propios.

--- tests/AccessHandlerTest.php
+++ tests/AccessHandlerTest.php
@@ -3,21 +3,50 @@
 namespace LuisLuciano\Components\Tests;
 
 use LuisLuciano\Components\AccessHandler as Access;
+use LuisLuciano\Components\Contracts\AuthenticatorContract;
+use LuisLuciano\Components\Models\User;
 use LuisLuciano\Components\Stubs\AuthenticatorStub;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class AccessHandlerTest extends TestCase
 {
+    /**
+     * This method is called after each test.
+     */
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    protected function getAuthenticatorMock(): AuthenticatorContract
+    {
+        $user = Mockery::mock(User::class);
+        $user->role = 'admin';
+
+        $auth = Mockery::mock(AuthenticatorContract::class);
+        $auth->expects()
+            ->check()
+            ->once()
+            ->andReturn(true);
+        $auth->expects()
+            ->user()
+            ->once()
+            ->andReturn($user);
+
+        return $auth;
+    }
     public function test_grant_access()
     {
-        $auth = new AuthenticatorStub();
+        $auth = $this->getAuthenticatorMock();
         $access = new Access($auth);
+
         $this->assertTrue($access->check('admin'));
     }
 
     public function test_deny_access()
     {
-        $auth = new AuthenticatorStub();
+        $auth = $this->getAuthenticatorMock();
         $access = new Access($auth);
         $this->assertFalse($access->check('editor'));
     }
